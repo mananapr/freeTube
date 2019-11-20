4c2d3aac9430c3cb9c4ab621636e796365b18e9a
mananapr <manny011@live.com>
2019-11-20 15:38:19 +0530

    Added a basic video template and route. Added server prefix variable for search and index routes

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from app import app
 import requests
 import json
 
+SERVER_DOMAIN_NAME = 'http://127.0.0.1:8000'
 API_ADDR = 'http://127.0.0.1'
 API_PORT = '5000'
 API_URL = API_ADDR + ":" + API_PORT + "/api/v1/"
@@ -12,7 +13,7 @@ API_URL = API_ADDR + ":" + API_PORT + "/api/v1/"
 def index():
     response = requests.get(API_URL+"explore")
     results = json.loads(response.text)["videos"]
-    return render_template('index.html', title='Home', results=results)
+    return render_template('index.html', title='Home', results=results, baseaddr=SERVER_DOMAIN_NAME)
 
 @app.route('/search', methods=['GET'])
 def search():
@@ -20,8 +21,18 @@ def search():
     if page <= 0:
         page = 1
     query = request.args.get('search')
-    print(query)
     payload = {'query':query}
     response = requests.post(API_URL+"search?page="+str(page), json=payload)
     results = json.loads(response.text)['videos']
-    return render_template('search.html', title='Search', page=page, results=results, prev_page="/search?search={}&page={}".format(query,str(page-1)), next_page="/search?search={}&page={}".format(query,str(page+1)))
+    return render_template('search.html', title='Search', page=page, results=results, prev_page="/search?search={}&page={}".format(query,str(page-1)), next_page="/search?search={}&page={}".format(query,str(page+1)), baseaddr=SERVER_DOMAIN_NAME)
+
+@app.route('/watch', methods=['GET'])
+def watch():
+    videoID = request.args.get('v', default="error")
+    if videoID == "error":
+        return "Please specify a valid video"
+    ytURL = "https://youtube.com/watch?v=" + videoID
+    payload = {'url':ytURL}
+    response = requests.post(API_URL+"urlinfo", json=payload)
+    results = json.loads(response.text)
+    return render_template("video.html", title=results["title"], video=results, baseaddr=SERVER_DOMAIN_NAME)
